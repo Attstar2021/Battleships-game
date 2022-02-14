aa1886a0458a7101d0810af3c86bfe19177c81af
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-02-14 21:34:20 +0000

    Define validate_board function

--- run.py
+++ run.py
@@ -13,8 +13,25 @@ game_over = False
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 
-def validate_board():
+def validate_board(x1, x2, y1, y2):
+    """
+    To place ship inside the grid.
+    """
+    global ship_placement
+    global board
 
+    all_valid = True
+    for r in range(x1, x2):
+        for c in range(y1, y2):
+            if board[r][c] != ".":
+                all_valid = False
+                break
+    if all_valid:
+        ship_placement.append([x1, x2, y1, y2])
+        for r in range(x1, x2):
+            for c in range(y1,y2):
+                board[r][c] = "O"
+    return all_valid
 
 def place_ship():
 
