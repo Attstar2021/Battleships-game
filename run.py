8f06d29b0165573e4186abded45f2ee0a8b7704c
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-02-14 21:18:16 +0000

    Create all function names

--- run.py
+++ run.py
@@ -1,3 +1,46 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import randon
+import time
+"""
+Declared global veriable for battleships game.
+"""
+board =[[]]
+board_size = 10
+num_of_ships = 5
+ship_placement = [[]]
+fire_left = 10
+ship_sunk = 0
+game_over = False
+alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+
+def validate_board():
+
+
+def place_ship():
+
+
+def create_board():
+
+
+def print_board():
+
+
+def fire_placement():
+
+
+def check_for_ship_sunk():
+
+
+def shoot_a_fire():
+
+
+def gameover():
+
+
+def main():
+
+
+
+
+
+
