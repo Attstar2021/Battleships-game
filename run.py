6deef9b701831a5d917acf8154278b16f3577aa5
Attstar2021 <attiyarasheed923@yahoo.co.uk>
2022-02-15 15:35:35 +0000

    Define create_board function

--- run.py
+++ run.py
@@ -12,6 +12,36 @@ ship_sunk = 0
 game_over = False
 alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+def create_board():
+    """
+    to create a board and to place ships of 
+    different sizes on gird.
+    """
+    global board_size
+    global board
+    global num_of_ships
+    global ship_placement
+
+    random.seed(time.time())
+    rows, cols = (board_sizem board_size)
+    board = []
+    row = []
+    for r in range(rows):
+        row.append(".")
+    for c in range(cols):
+        board.append(row)
+    num_of_ships_placed = 0
+    ship_placement = []
+
+    while num_of_ships_placed != num_of_ships
+        random_row = random.randint(0,rows - 1)
+        random_col = random.randint(0, cols - 1)
+        direction = random.choice(["left", "right", "up", "down"])
+        ship_size = random.randint(1, 3)
+        if place_ship(random_row, random_col, direction, ship_size):
+            num_of_ships_placed += 1
+    
+
 
 def validate_board(x1, x2, y1, y2):
     """
@@ -36,7 +66,7 @@ def validate_board(x1, x2, y1, y2):
 def place_ship():
 
 
-def create_board():
+
 
 
 def print_board():
@@ -44,11 +74,9 @@ def print_board():
 
 def fire_placement():
 
-
+def shoot_a_fire():
 def check_for_ship_sunk():
 
-
-def shoot_a_fire():
 
 
 def gameover():
